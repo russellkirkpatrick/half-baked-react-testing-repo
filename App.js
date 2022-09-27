e7ca618ba0187ee324ddaec39b5fd16e627baab6
Russell Kirkpatrick <109391715+russellkirkpatrick@users.noreply.github.com>
2022-09-27 19:50:55 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import BlogNav from './components/BlogNav';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Note from './components/Note';
+
+function App() {
+  return (
+    <>
+    
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    <Note />
+    
+    </>
+  );
+}
+
+export default App;
